cd998a711e8edc54657257ed5dd4204b9c722e04
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-11-14 10:00:46 +0200

    fix: add / in package json homepage

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { App } from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/currency-conversion/">
+    <BrowserRouter basename="/currency-conversion">
       <App />
     </BrowserRouter>
   </React.StrictMode>
